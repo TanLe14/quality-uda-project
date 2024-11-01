bebc82d24f06826a2e795c037b3ab2edce2286a8
tanle14 <lexuantan2000@gmail.com>
2024-11-01 16:02:09 +0700

    update

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -27,14 +27,14 @@ module "network" {
 }
 
 module "nsg-test" {
-  source           = "../../modules/networksecuritygroup"
-  location         = var.location
-  application_type = var.application_type
-  resource_type    = "NSG"
-  resource_group   = var.resource_group
-  subnet_id        = module.network.subnet_id_test
+  source              = "../../modules/networksecuritygroup"
+  location            = var.location
+  application_type    = var.application_type
+  resource_type       = "NSG"
+  resource_group      = var.resource_group
+  subnet_id           = module.network.subnet_id_test
   address_prefix_test = var.address_prefix_test
-  depends_on = [ module.network ]
+  depends_on          = [module.network]
 }
 module "appservice" {
   source           = "../../modules/appservice"
@@ -52,14 +52,14 @@ module "publicip" {
 }
 
 module "vm" {
-  source = "../../modules/vm"
+  source                 = "../../modules/vm"
   network_interface_name = var.virtual_network_interface_name
-  subnet_id = module.network.subnet_id_test
-  public_ip_address_id= module.publicip.public_ip_address_id
-  vm_name= var.vm_name
-  location = var.location
-  resource_group_name = var.resource_group
-  admin_user= var.admin_user
-  admin_ssh_key = var.admin_ssh_key
-  depends_on = [ module.network ]
+  subnet_id              = module.network.subnet_id_test
+  public_ip_address_id   = module.publicip.public_ip_address_id
+  vm_name                = var.vm_name
+  location               = var.location
+  resource_group_name    = var.resource_group
+  admin_user             = var.admin_user
+  admin_ssh_key          = var.admin_ssh_key
+  depends_on             = [module.network]
 }
